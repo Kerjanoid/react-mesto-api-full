fefd1113ebdeca6ccc60f4a2985713f1c3d24b3c
Vyacheslav Bardakov <Kerjanoid@yandex.ru>
2021-08-08 16:02:14 +0300

    PR15. Ready for check

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,91 @@
+class Api {
+  constructor({baseUrl, headers}) {
+    this._baseUrl = baseUrl
+    this._headers = headers
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json()
+    }
+    return Promise.reject(`Ошибка: ${res.status}`)
+  }
+
+  getUserInformation() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(this._checkResponse)
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(this._checkResponse)
+  }
+
+  editProfile(userData) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify(userData)
+    })
+      .then(this._checkResponse)
+  }
+
+  editAvatar(userAvatar) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify(userAvatar)
+    })
+      .then(this._checkResponse);
+  }
+
+  addCard(cardData) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify(cardData)
+    })
+      .then(this._checkResponse)
+  }
+
+  removeCard(cardID) {
+    return fetch(`${this._baseUrl}/cards/${cardID}`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+      .then(this._checkResponse)
+  }
+
+  changeLikeCardStatus(cardId, isLiked) {
+    const putLike = {
+      headers: this._headers,
+      method:'PUT',
+    }
+    const delLike = {
+      headers: this._headers,
+      method:'DELETE',
+    }
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, isLiked ? delLike : putLike)
+      .then(this._checkResponse)
+  }
+}
+
+export default new Api({
+  baseUrl: `express-mesto.kerjanoid.nomoredomains.monster`,
+  headers: {
+      authorization: `Bearer ${localStorage.getItem('token')}`,
+      'Content-Type': 'application/json'
+    }
+})
